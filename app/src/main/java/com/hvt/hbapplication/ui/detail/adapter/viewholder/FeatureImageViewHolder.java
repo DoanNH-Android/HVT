28b04804e558f82c232fbf13de67d9a387b5da65
Hado <Hado@Macbook-3.local>
2017-07-04 22:50:46 +0700

    add adapter and viewholder of detail screen

---
+++ app/src/main/java/com/hvt/hbapplication/ui/detail/adapter/viewholder/FeatureImageViewHolder.java
@@ -0,0 +1,36 @@
+package com.hvt.hbapplication.ui.detail.adapter.viewholder;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.hvt.hbapplication.R;
+import com.hvt.hbapplication.model.Image;
+import com.hvt.hbapplication.ui.BaseViewHolder;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Hado on 7/4/17.
+ */
+
+public class FeatureImageViewHolder extends BaseViewHolder<Image> {
+
+    @BindView(R.id.iv_image)
+    public ImageView ivImage;
+
+    public FeatureImageViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void bindData(Image data) {
+        Glide.with(itemView.getContext()).load(data.getImageUrl()).into(ivImage);
+    }
+
+    @Override
+    public void bindView() {
+        ButterKnife.bind(this, itemView);
+    }
+}
